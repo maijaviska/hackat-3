cb2af560ee81a960ce65b50c41f94289f03efdc3
Lauresmi <esmite.laura@gmail.com>
2022-02-12 14:05:06 +0200

    added Billing address

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
+import Carousel from "../Components/Carousel";
+
 function Home() {
     return (
         <div className="row">
              <div className="col">
                 <h1>This is home</h1>
+                <Carousel />
             </div>
-
         </div>
     )
 }
