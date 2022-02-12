ef03ec1f14f8c1c2495e1f29027664a4c941bdb1
valeriastorozhuk <90319097+valeriastorozhuk@users.noreply.github.com>
2022-02-12 13:57:40 +0200

    all pages created

---
+++ src/Pages/Cart.js
@@ -0,0 +1,12 @@
+function Cart() {
+    return (
+        <div className="row">
+             <div className="col">
+                <h1>This is Cart</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Cart;
---
+++ src/Pages/ProductPage.js
@@ -0,0 +1,12 @@
+function ProductPage() {
+    return (
+        <div className="row">
+             <div className="col">
+                <h1>This is Product Page</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProductPage;
