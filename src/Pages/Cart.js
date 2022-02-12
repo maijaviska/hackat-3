f9051bbd80cabb5bf15b928e62dec79f1da9eed1
maijaviska <48285008+maijaviska@users.noreply.github.com>
2022-02-12 15:01:08 +0200

    added links

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,8 +1,11 @@
+import ShippingAdress from "../Components/ShippingAddress";
+
 function Cart() {
     return (
         <div className="row">
              <div className="col">
                 <h1>This is Cart</h1>
+                <ShippingAdress />
             </div>
 
         </div>
