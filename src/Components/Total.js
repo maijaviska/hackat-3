a9d82c87138229d925061873d3622d83751c8cfc
maijaviska <48285008+maijaviska@users.noreply.github.com>
2022-02-12 15:53:09 +0200

    update cart

---
+++ src/Components/Total.js
@@ -0,0 +1,25 @@
+import "./ShippingAddress.css"
+
+function Total() {
+    return (
+        <div className="container-fluid">
+            <form>
+                <div class="row m-3 ">
+                    <div class="col-md-6 px-3 pameginam">
+                        <div class="form-outline">
+                            <div class="col-xxl-11 col-xl-10 col-md-10 col-sm-9">
+                                <h5 for="username">Shipping address</h5><br />
+                                <textarea id="username-input" name="input-message" rows="5" cols="76"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex justify-content-end">
+                    <button class="btn btn-success address-btn" id="address-button" onclick="addAddress()">Add Address</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Total;
