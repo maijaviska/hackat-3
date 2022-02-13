a3a62e97ee598e5b48e16103d858c4b5200ff241
Lauresmi <esmite.laura@gmail.com>
2022-02-13 10:06:15 +0200

    updated tabs in cart

--- src/Components/ShippingAddress.js
+++ src/Components/ShippingAddress.js
@@ -4,18 +4,15 @@ function ShippingAddress() {
     return (
         <div className="container-fluid">
             <form>
-                <div class="row m-3 ">
-                    <div class="col-md-6 px-3 pameginam">
+                <div class="row m-3">
+                    <div class="px-3 col-for-ship-area">
                         <div class="form-outline">
-                            <div class="col-xxl-11 col-xl-10 col-md-10 col-sm-9">
+                            <div class="parent-for-textarea">
                                 <h5 for="username">Shipping address</h5><br />
-                                <textarea id="username-input" name="input-message" rows="5" cols="76"></textarea>
+                                <textarea id="username-input" name="input-message" rows="" cols=""></textarea>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-6 d-flex justify-content-end">
-                    <button class="btn btn-success address-btn" id="address-button" onclick="addAddress()">Add Address</button>
                 </div>
             </form>
         </div>
