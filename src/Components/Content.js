74b96106da9960908fea76b20034df643debd626
valeriastorozhuk <90319097+valeriastorozhuk@users.noreply.github.com>
2022-02-13 15:40:07 +0200

    one-category

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,6 +3,7 @@ import Cart from "../Pages/Cart";
 import Categories from "../Pages/Categories";
 import Home from "../Pages/Home";
 import OneCategory from "../Pages/OneCategory";
+import ProductPage from "../Pages/ProductPage";
 
 function Content() {
     return (
@@ -11,6 +12,7 @@ function Content() {
                 <Route path="/" element={<Home />} />
                 <Route path="/categories" element={<Categories />} />
                 <Route path="/cakes" element={<OneCategory />} />
+                <Route path="/product-page" element={<ProductPage />} />
                 <Route path="/cart" element={<Cart />} />
             </Routes>
         </div>
